8fcd9e177840874d068340197be0ddc612b9f475
jeko <chelidzejeko9123741jeko@gmail.com>
2021-07-31 13:18:37 +0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,48 @@ $(".next").click(function(){
   });
   $(".burger").click(function(){
     $(".burger-menu").toggleClass("active");
+    $(".burger-line1").toggleClass("close");
+    $(".burger-line2").toggleClass("remove");
+    $(".burger-line3").toggleClass("close1");
+    $("body").toggleClass("overflow-hidden");
   });
+
+
+  $('.slider-box').slick({
+    infinite:false,
+    speed:500,
+    // autoplay:true,
+    draggable:false,
+    // autoplaySpeed:5000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow:$('#back-arr'),
+    nextArrow:$('#next-arr'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  
