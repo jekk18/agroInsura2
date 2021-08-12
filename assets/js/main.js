2f562e71ec32087158b8bad0c5f2969e433698ca
jeko <chelidzejeko9123741jeko@gmail.com>
2021-08-12 17:29:24 +0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,132 @@
-$(".next").click(function(){
-    $(".line1").css("width", "100%");
-    $(".icon-next").css("color", "#C8C8C8");
-    $(".icon-back").css("color", "#7AA259");
-  });
-  $(".back").click(function(){
-    $(".line1").css("width", "49%");
-    $(".icon-next").css("color", "#7AA259");
-    $(".icon-back").css("color", "#C8C8C8");
-  });
-  $(".burger").click(function(){
-    $(".burger-menu").toggleClass("active");
-    $(".burger-line1").toggleClass("close");
-    $(".burger-line2").toggleClass("remove");
-    $(".burger-line3").toggleClass("close1");
-    $("body").toggleClass("overflow-hidden");
-  });
+$(".burger").click(function(){
+  $(".burger-menu").toggleClass("active");
+  $(".burger-line1").toggleClass("close");
+  $(".burger-line2").toggleClass("remove");
+  $(".burger-line3").toggleClass("close1");
+  $(".burger").toggleClass("fix");
+});
 
 
-  $('.slider-box').slick({
-    infinite:false,
-    speed:500,
-    // autoplay:true,
-    draggable:false,
-    // autoplaySpeed:5000,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow:$('#back-arr'),
-    nextArrow:$('#next-arr'),
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
+
+
+$('.slider').slick({
+  infinite:false,
+  speed:500,
+  // autoplay:true,
+  draggable:true,
+  // autoplaySpeed:5000,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow:$('#back-arr'),
+  nextArrow:$('#next-arr'),
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-  
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.news-slider').slick({
+  infinite:false,
+  speed:500,
+  // autoplay:true,
+  draggable:true,
+  // autoplaySpeed:5000,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow:$('#back-arr'),
+  nextArrow:$('#next-arr'),
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+$('.news2-slider').slick({
+  infinite:false,
+  speed:500,
+  // autoplay:true,
+  draggable:true,
+  // autoplaySpeed:5000,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow:$('#back-arr'),
+  nextArrow:$('#next-arr'),
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// if($(window).width() < 767)
+// {
+//    // change functionality for smaller screens
+// } else {
+//    // change functionality for larger screens
+// }
